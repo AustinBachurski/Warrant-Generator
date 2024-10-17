24a7ae9b8cade2d3d6447838a559eb90fd323139
Austin Bachurski <austinbachurski@gmail.com>
2024-10-16 21:22:03 -0600

    Use community toolkit for view model.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Media;
-using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Warrant_Generator.DTOs;
 using WarrantGenerator.Document;
 using WarrantGenerator.DTOs;
@@ -7,289 +8,133 @@ using WarrantGenerator.Interfaces;
 
 namespace WarrantGenerator.ViewModels;
 
-public partial class MainWindowViewModel : INotifyPropertyChanged
+public partial class MainWindowViewModel : ObservableObject
 {
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    private IBrush _organizationBorder = Brushes.Transparent;
-    private string _organizationText = string.Empty;
-    private IBrush _officerNameBorder = Brushes.Transparent;
-    private string _officerNameText = string.Empty;
-    private IBrush _officerTitleBorder = Brushes.Transparent;
-    private string _officerTitleSelection = string.Empty;
-    private IBrush _customOfficerTitleBorder = Brushes.Transparent;
-    private string _customOfficerTitleText = "Enter Title";
-    private bool _customOfficerTitleVisibility = false;
-    private IBrush _employmentDurationBorder = Brushes.Transparent;
-    private string _employmentDurationText = string.Empty;
-    private IBrush _targetAddressBorder = Brushes.Transparent;
-    private string _targetAddressText = string.Empty;
-    private IBrush _targetDescriptionBorder = Brushes.Transparent;
-    private string _targetDescriptionText = string.Empty;
-    private IBrush _searchWarrantReasonBorder = Brushes.Transparent;
-    private string _searchWarrantReasonText = string.Empty;
-    private IBrush _searchWarrantContentBorder = Brushes.Transparent;
-    private string _searchWarrantContentText = string.Empty;
-    private IBrush _outputFileNameBorder = Brushes.Transparent;
-    private string _outputFileNameText = string.Empty;
-
-    public IBrush OrganizationBorder
-    {
-        get => _organizationBorder;
-        set
-        {
-            if (_organizationBorder != value)
-            {
-                _organizationBorder = value;
-                OnPropertyChanged(nameof(OrganizationBorder));
-            }
-        }
-    }
-
-    public string OrganizationText
-    { 
-        get => _organizationText;
-        set
-        {
-            _organizationText = value;
-            OrganizationBorder = Brushes.Transparent;
-        }
-    }
-
-    public IBrush OfficerNameBorder
-    {
-        get => _officerNameBorder;
-        set
-        {
-            if (_officerNameBorder != value)
-            {
-                _officerNameBorder = value;
-                OnPropertyChanged(nameof(OfficerNameBorder));
-            }
-        }
-    }
-
-    public string OfficerNameText
-    {
-        get => _officerNameText;
-        set
-        {
-            _officerNameText = value;
-            OfficerNameBorder = Brushes.Transparent;
-        }
-    }
-
-    public IBrush OfficerTitleBorder
-    {
-        get => _officerTitleBorder;
-        set
-        {
-            if (_officerTitleBorder != value)
-            {
-                _officerTitleBorder = value;
-                OnPropertyChanged(nameof(OfficerTitleBorder));
-            }
-        }
-    }
-
+    public string[] DurationTypes { get; set; } = { "years", "year", "months", "month", "days", "day" };
+    public string DurationTypeSelection { get; set; } = "years";
     public string[] OfficerTitles { get; set; } = { "Captain", "Chief", "Detective", "Detective Sergeant", "Lieutenant", "Master Patrol Officer", "Officer", "Sergeant", "Other" };
 
-    public string OfficerTitleSelection
-    {
-        get => _officerTitleSelection;
-        set
-        {
-            if (value == "Other")
-            {
-                CustomOfficerTitleVisibility = true;
-            }
-            else
-            {
-                CustomOfficerTitleVisibility = false;
-            }
+    [ObservableProperty]
+    private IBrush _organizationBorder = Brushes.Transparent;
 
-            _officerTitleSelection = value;
-            OfficerTitleBorder = Brushes.Transparent;
-            CustomOfficerTitleBorder = Brushes.Transparent;
-        }
+    [ObservableProperty]
+    private string _organizationText = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _officerNameBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _officerNameText = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _officerTitleBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _officerTitleSelection = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _customOfficerTitleBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _customOfficerTitleText = "Enter Title";
+
+    [ObservableProperty]
+    private bool _customOfficerTitleVisibility = false;
+
+    [ObservableProperty]
+    private IBrush _employmentDurationBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _employmentDurationText = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _targetAddressBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _targetAddressText = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _targetDescriptionBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _targetDescriptionText = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _searchWarrantReasonBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _searchWarrantReasonText = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _searchWarrantContentBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _searchWarrantContentText = string.Empty;
+
+    [ObservableProperty]
+    private IBrush _outputFileNameBorder = Brushes.Transparent;
+
+    [ObservableProperty]
+    private string _outputFileNameText = string.Empty;
+
+    partial void OnOrganizationTextChanged(string value)
+    {
+        OrganizationBorder = Brushes.Transparent;
     }
 
-    public IBrush CustomOfficerTitleBorder
+    partial void OnOfficerNameTextChanged(string value)
     {
-        get => _customOfficerTitleBorder;
-        set
-        {
-            if (_customOfficerTitleBorder != value)
-            {
-                _customOfficerTitleBorder = value;
-                OnPropertyChanged(nameof(CustomOfficerTitleBorder));
-            }
-        }
+        OfficerNameBorder = Brushes.Transparent;
     }
 
-    public string CustomOfficerTitleText
+    partial void OnOfficerTitleSelectionChanged(string value)
     {
-        get => _customOfficerTitleText;
-        set
+        if (value == "Other")
         {
-            _customOfficerTitleText = value;
-            CustomOfficerTitleBorder = Brushes.Transparent;
+            CustomOfficerTitleVisibility = true;
         }
+        else
+        {
+            CustomOfficerTitleVisibility = false;
+        }
+
+        OfficerTitleBorder = Brushes.Transparent;
+        CustomOfficerTitleBorder = Brushes.Transparent;
     }
 
-    public bool CustomOfficerTitleVisibility
+    partial void OnCustomOfficerTitleTextChanged(string value)
     {
-        get => _customOfficerTitleVisibility;
-        set
-        {
-            if (_customOfficerTitleVisibility != value)
-            {
-                _customOfficerTitleVisibility = value;
-                OnPropertyChanged(nameof(CustomOfficerTitleVisibility));
-            }
-        }
+        CustomOfficerTitleBorder = Brushes.Transparent;
     }
 
-    public IBrush EmploymentDurationBorder
+    partial void OnEmploymentDurationTextChanged(string value)
     {
-        get => _employmentDurationBorder;
-        set
-        {
-            if (_employmentDurationBorder != value)
-            {
-                _employmentDurationBorder = value;
-                OnPropertyChanged(nameof(EmploymentDurationBorder));
-            }
-        }
+        EmploymentDurationBorder = Brushes.Transparent;
     }
 
-    public string EmploymentDurationText
+    partial void OnTargetAddressTextChanged(string value)
     {
-        get => _employmentDurationText;
-        set
-        {
-            _employmentDurationText = value;
-            EmploymentDurationBorder = Brushes.Transparent;
-        }
+        TargetAddressBorder = Brushes.Transparent;
     }
 
-    public string[] DurationTypes { get; set; } = { "years", "year", "months", "month", "days", "day" };
-
-    public string DurationTypeSelection { get; set; } = "years";
-
-    public IBrush TargetAddressBorder
+    partial void OnTargetDescriptionTextChanged(string value)
     {
-        get => _targetAddressBorder;
-        set
-        {
-            if (_targetAddressBorder != value)
-            {
-                _targetAddressBorder = value;
-                OnPropertyChanged(nameof(TargetAddressBorder));
-            }
-        }
+        TargetDescriptionBorder = Brushes.Transparent;
     }
 
-    public string TargetAddressText
+    partial void OnSearchWarrantReasonTextChanged(string value)
     {
-        get => _targetAddressText;
-        set
-        {
-            _targetAddressText = value;
-            TargetAddressBorder = Brushes.Transparent;
-        }
+        SearchWarrantReasonBorder = Brushes.Transparent;
     }
 
-    public IBrush TargetDescriptionBorder
+    partial void OnSearchWarrantContentTextChanged(string value)
     {
-        get => _targetDescriptionBorder;
-        set
-        {
-            if (_targetDescriptionBorder != value)
-            {
-                _targetDescriptionBorder = value;
-                OnPropertyChanged(nameof(TargetDescriptionBorder));
-            }
-        }
+        SearchWarrantContentBorder = Brushes.Transparent;
     }
 
-    public string TargetDescriptionText
+    partial void OnOutputFileNameTextChanged(string value)
     {
-        get => _targetDescriptionText;
-        set
-        {
-            _targetDescriptionText = value;
-            TargetDescriptionBorder = Brushes.Transparent;
-        }
-    }
-
-    public IBrush SearchWarrantReasonBorder
-    {
-        get => _searchWarrantReasonBorder;
-        set
-        {
-            if (_searchWarrantReasonBorder != value)
-            {
-                _searchWarrantReasonBorder = value;
-                OnPropertyChanged(nameof(SearchWarrantReasonBorder));
-            }
-        }
-    }
-
-    public string SearchWarrantReasonText
-    {
-        get => _searchWarrantReasonText;
-        set
-        {
-            _searchWarrantReasonText = value;
-            SearchWarrantReasonBorder = Brushes.Transparent;
-        }
-    }
-
-    public IBrush SearchWarrantContentBorder
-    {
-        get => _searchWarrantContentBorder;
-        set
-        {
-            if (_searchWarrantContentBorder != value)
-            {
-                _searchWarrantContentBorder = value;
-                OnPropertyChanged(nameof(SearchWarrantContentBorder));
-            }
-        }
-    }
-
-    public string SearchWarrantContentText
-    {
-        get => _searchWarrantContentText;
-        set
-        {
-            _searchWarrantContentText = value;
-            SearchWarrantContentBorder = Brushes.Transparent;
-        }
-    }
-
-    public IBrush OutputFileNameBorder
-    {
-        get => _outputFileNameBorder;
-        set
-        {
-            if (_outputFileNameBorder != value)
-            {
-                _outputFileNameBorder = value;
-                OnPropertyChanged(nameof(OutputFileNameBorder));
-            }
-        }
-    }
-
-    public string OutputFileNameText
-    {
-        get => _outputFileNameText;
-        set
-        {
-            _outputFileNameText = value;
-            OutputFileNameBorder = Brushes.Transparent;
-        }
+        OutputFileNameBorder = Brushes.Transparent;
     }
 
     public IReplacementData[] AssembleData()
@@ -312,6 +157,7 @@ public partial class MainWindowViewModel : INotifyPropertyChanged
         return data;
     }
 
+    [RelayCommand]
     public void GenerateWarrantDocument()
     {
         if (!InputsAreValid())
@@ -388,11 +234,6 @@ public partial class MainWindowViewModel : INotifyPropertyChanged
         }
 
         return result;
-    }
-
-    private void OnPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
 
