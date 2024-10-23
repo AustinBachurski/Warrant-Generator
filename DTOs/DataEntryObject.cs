03ffcef7d54b8afa5f0304bded07f47f3b9ef825
abachurski <austinbachurski@gmail.com>
2024-10-23 17:09:06 -0600

    Refactor away from Xceed DocX.

---
+++ DTOs/DataEntryObject.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using WarrantGenerator.ViewModels;
+
+namespace WarrantGenerator.DTOs;
+
+public class DataEntryObject(MainWindowViewModel viewModel)
+{
+    public string ApplicationDate { get; } = FormattedDateString();
+    public string Crimes { get; } = CrimesAsString(viewModel.Crimes);
+    public string CrimeGrammar { get; } = viewModel.Crimes.Count == 1 ? "crime" : "crimes";
+    public string District { get; } = viewModel.CourtDistrictSelection;
+    public string DurationType { get; } = viewModel.DurationTypeSelection;
+    public string EmploymentDuration { get; } = viewModel.EmploymentDurationText;
+    public string OfficerGender { get; } = viewModel.MaleChecked ? "he" : "she";
+    public string OfficerGrammar { get; } = CorrectGrammar(viewModel.OfficerTitleSelection.First());
+    public string OfficerName { get; } = viewModel.OfficerNameText;
+    public string OfficerTitle { get; } = GetOfficerTitle(viewModel);
+    public string Organization { get; } = viewModel.OrganizationText;
+    public string ProbableCause { get; } = viewModel.ProbableCauseText;
+    public string SeizableProperty { get; } = viewModel.SeizablePropertyText;
+    public string SuspectDateOfBirth { get; } = string.Empty;
+    public string SuspectDriversLicense { get; } = string.Empty;
+    public string SuspectName { get; } = string.Empty;
+    public string TargetAddress { get; } = viewModel.TargetAddressText;
+    public string TargetDescription { get; } = viewModel.TargetDescriptionText;
+
+    private static string GetOfficerTitle(MainWindowViewModel viewModel)
+    {
+        if (viewModel.CustomOfficerTitleVisibility)
+        {
+            return viewModel.CustomOfficerTitleText;
+        }
+        return viewModel.OfficerTitleSelection;
+    }
+
+   private static string CorrectGrammar(char c)
+    {
+        return char.ToUpper(c) switch
+        {
+            'A' or 'E' or 'I' or 'O' or 'U' => "an",
+            _ => "a",
+        };
+    }
+
+    private static string FormattedDateString()
+    {
+        var now = DateTime.Now;
+        return $"{now.Day}{GetDayOrdinal(now)} day of {now:MMMM, yyyy}";
+    }
+
+    private static string GetDayOrdinal(DateTime date)
+    {
+        switch (date.Day)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return "th";
+        }
+
+        switch (date.Day % 10)
+        {
+            case 1:
+                return "st";
+
+            case 2:
+                return "nd";
+
+            case 3:
+                return "rd";
+
+            default:
+                return "th";
+        }
+    }
+
+    private static string CrimesAsString(ObservableCollection<MCACrime> crimesList)
+    {
+        StringBuilder builder = new();
+
+        const string prefix = "M.C.A. § ";
+        const string space = " (";
+        const string suffix = "), ";
+        int contentLength = prefix.Length + space.Length + suffix.Length;
+
+        foreach (MCACrime each in crimesList)
+        {
+            builder.Append(prefix);
+            builder.Append(each.Code);
+            builder.Append(space);
+            builder.Append(each.Description);
+            builder.Append(suffix);
+        }
+
+        if (crimesList.Count > 1)
+        {
+            builder.Insert(StartingPositionOfLastEntry(builder, contentLength, crimesList), "and ");
+        }
+
+        // Trailing ", " is intentional.
+        return builder.ToString();
+    }
+
+    private static int StartingPositionOfLastEntry(StringBuilder builder, int contentLength, ObservableCollection<MCACrime> crimesList)
+    {
+        contentLength += crimesList.Last().Code.Length;
+        contentLength += crimesList.Last().Description.Length;
+
+        return builder.Length - contentLength;
+    }
+}
---
+++ DocumentGeneration/DocumentGenerator.cs
@@ -0,0 +1,95 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using WarrantGenerator.DTOs;
+
+namespace WarrantGenerator.DocumentGeneration;
+
+public class DocumentGenerator(string fileName)
+{
+    private Body _body = new();
+    private readonly string _outputFile = $"C:/Temp/{fileName}.docx";
+
+    public string GenerateDocument(DataEntryObject data)
+    {
+        using var newDocument = WordprocessingDocument.Create(
+            _outputFile, DocumentFormat.OpenXml.WordprocessingDocumentType.Document);
+
+        var document = newDocument.AddMainDocumentPart();
+        document.Document = new Document();
+        SetDocumentFormatting(document);
+
+        InsertBoilerplate(data.District);
+
+        // Data insertion.
+
+
+        document.Document.Append(_body);
+        document.Document.Save();
+        return _outputFile;
+    }
+
+    private void AppendFormattedLine(string line)
+    {
+        var paragraph = new Paragraph();
+        var run = new Run();
+
+        foreach (var content in line.Split('\t'))
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                run.Append(new TabChar());
+            }
+            else
+            {
+                run.Append(new Text(content));
+            }
+        }
+
+        paragraph.Append(run);
+        _body.Append(paragraph);
+    }
+    
+    private void InsertBoilerplate(string district)
+    {
+        var boilerplate = new[]
+        {
+            $"IN THE {district}, COUNTY OF FLATHEAD, STATE OF MONTANA",
+            "STATE OF MONTANA\t\t\t\t\t\t)",
+            "\t\t\tPlaintiff,\t\t\t\t)",
+            "\t\t\t\t-vs-\t\t\t\t\t)\t\tAPPLICATION FOR",
+            "\t\t\tDefendant,\t\t\t\t)\t\tSEARCH WARRANT",
+            "",
+        };
+
+        foreach (var line in boilerplate)
+        {
+            AppendFormattedLine(line);
+        }
+    }
+
+    private void SetDocumentFormatting(MainDocumentPart document)
+    {
+        var paragraphProperties = new ParagraphProperties();
+        paragraphProperties.Append(new Justification() { Val = JustificationValues.Both });
+
+        var runProperties = new RunProperties();
+        runProperties.Append(new RunFonts() { Ascii = "Courier New", HighAnsi = "Courier New" });
+        runProperties.Append(new FontSize() { Val = "24" });  // Val / 2 == font size.
+
+        var documentStyle = document.AddNewPart<StyleDefinitionsPart>();
+        documentStyle.Styles = new Styles();
+
+        var defaultStyle = new Style()
+        {
+            Type = StyleValues.Paragraph,
+            StyleId = "Normal",
+            Default = true,
+            StyleName = new StyleName() { Val = "Normal" },
+        };
+
+        defaultStyle.Append(paragraphProperties);
+        defaultStyle.Append(new StyleRunProperties(runProperties));
+        documentStyle.Styles.Append(defaultStyle);
+    }
+}
