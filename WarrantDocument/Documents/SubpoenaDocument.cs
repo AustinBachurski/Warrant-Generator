024fa14f852ec32595fd90ab496b0ce3b1b62224
abachurski <austinbachurski@gmail.com>
2025-01-17 15:03:05 -0700

    Prep document generation for content.

--- WarrantDocument/Documents/SubpoenaDocument.cs
+++ WarrantDocument/Documents/SubpoenaDocument.cs
@@ -1,27 +1,64 @@
 ﻿using WarrantGenerator.Constants;
 using WarrantGenerator.DTOs;
+using WarrantGenerator.WarrantDocument;
+using WarrantGenerator.WarrantDocument.Boilerplate;
 using WarrantGenerator.WarrantDocument.Formatters;
+
+using System.Collections.Generic;
 
 
 namespace WarrantGenerator.WarrantDocument.Documents;
 
 public class SubpoenaDocument(SubpoenaData data)
 {
-    public string GenerateDocument()
+    public string GenerateDocuments()
     {
-        using var doc = new DocxDocument(_data.OutputFile + Extension.Docx);
+        List<string> documents = [];
+
+        if (_data.GenerateWarrantApplicationDocument)
+        {
+            documents.Add(GenerateApplicationDocument());
+        }
+
+        if (_data.GenerateWarrantDocument)
+        {
+            documents.Add(GenerateSearchWarrantDocument());
+        }
+
+        return string.Join("\n\t", documents);
+    }
+
+    private string GenerateApplicationDocument()
+    {
+        const string suffix = " - APPLICATION";
+
+        using var doc = new DocxDocument(_data.OutputFile + suffix + Extension.Docx);
 
         /* Document Content
-        *****************************************/ 
+        *****************************************/
 
         /***************************************** 
          * End Document Content*/
 
         doc.FinalizeDocument();
-        return _data.OutputFile + Extension.Docx;
+        return _data.OutputFile + suffix + Extension.Docx;
     }
 
-    private readonly SubpoenaData _data = data;
+    private string GenerateSearchWarrantDocument()
+    {
+        const string suffix = " - WARRANT";
 
+        using var doc = new DocxDocument(_data.OutputFile + suffix + Extension.Docx);
+
+        /* Document Content
+        *****************************************/
+
+        /***************************************** 
+         * End Document Content*/
+
+        doc.FinalizeDocument();
+        return _data.OutputFile + suffix + Extension.Docx;
+    }
+
+    private readonly SubpoenaData _data= data;
 }
-
